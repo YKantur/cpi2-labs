520039a5e10c38a61bf62df1b78095ba37c45f71
YKantur <144107607+YKantur@users.noreply.github.com>
2024-12-30 01:09:43 +0200

    added asyncFilter function
    added function that implements basic logic of filtration without debounce

--- task1.js
+++ task1.js
@@ -4,3 +4,35 @@ function simulateAsync(value, delay, callback) {
     callback(value > 2);
   }, delay);
 }
+
+function asyncFilter(array, asyncPredicate, finalCallback, debounceTime = 0) {
+  const results = []; // will store the results of the filtering
+  let completedCount = 0;
+  let debounceTimeout;
+
+  function handleCompletion(item, passes) {
+    if (passes) {
+      results.push(item);
+    }
+    completedCount++;
+
+    if (completedCount === array.length) {
+      // executes the final callback with or without a delay
+      if (debounceTime > 0) {
+        clearTimeout(debounceTimeout);
+        debounceTimeout = setTimeout(() => {
+          finalCallback(results);
+        }, debounceTime);
+      } else {
+        finalCallback(results);
+      }
+    }
+  }
+
+  array.forEach((item) => {
+    // initiates an asynchronous check for each element
+    asyncPredicate(item, (predicateResult) => {
+      handleCompletion(item, predicateResult);
+    });
+  });
+}
