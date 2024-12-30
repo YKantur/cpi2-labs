c7eab4851ce5425c90e76ccea02eec81aee259a9
YKantur <144107607+YKantur@users.noreply.github.com>
2024-12-30 02:14:14 +0200

    added base structure for createAsyncFilterFunction

--- task2.js
+++ task2.js
@@ -5,3 +5,17 @@ function simulateAsync(value, delay) {
     }, delay);
   });
 }
+
+function createAsyncFilter(useAsyncAwait) {
+  return function asyncFilter(
+    array,
+    asyncPredicate,
+    debounceTime = 0,
+    parallelism = Infinity
+  ) {
+    return new Promise((resolve) => {
+      const results = [];
+      let completedCount = 0;
+      let debounceTimeout;
+      let runningCount = 0;
+      const processingQueue = [...array];
