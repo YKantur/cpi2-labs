50cfaa626a9b007e4f6e251ae3b4033f9795f988
YKantur <144107607+YKantur@users.noreply.github.com>
2024-12-30 02:19:48 +0200

    added processNext to createAsyncFilter

--- task2.js
+++ task2.js
@@ -19,3 +19,16 @@ function createAsyncFilter(useAsyncAwait) {
       let debounceTimeout;
       let runningCount = 0;
       const processingQueue = [...array];
+            function processNext() {
+        if (processingQueue.length === 0 && runningCount === 0) {
+          if (debounceTime > 0) {
+            clearTimeout(debounceTimeout);
+            debounceTimeout = setTimeout(() => {
+              resolve(results);
+            }, debounceTime);
+          } else {
+            resolve(results);
+          }
+          return;
+        }
+
