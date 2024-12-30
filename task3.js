86679b5147c5381ab8c8ae752402cba4fb2d9ed7
YKantur <144107607+YKantur@users.noreply.github.com>
2024-12-30 20:13:41 +0200

    Add simulateAsync with signal handling

--- task3.js
+++ task3.js
@@ -1,1 +1,16 @@
-
+function simulateAsync(value, delay, signal) {
+    return new Promise((resolve, reject) => {
+      if (signal?.aborted) {
+        return reject(new Error("Aborted"));
+      }
+  
+      const timeoutId = setTimeout(() => {
+        resolve(value > 2);
+      }, delay);
+  
+      signal?.addEventListener("abort", () => {
+        clearTimeout(timeoutId);
+        reject(new Error("Aborted"));
+      });
+    });
+  }
