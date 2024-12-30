3e5f4db030df17f7744c02284a1a4aec2253d27c
YKantur <144107607+YKantur@users.noreply.github.com>
2024-12-30 21:27:57 +0200

    Added simulateAsync and generateData functions

--- task4.js
+++ task4.js
@@ -1,1 +1,15 @@
+function simulateAsync(value, delay, signal) {
+  return new Promise((resolve, reject) => {
+    if (signal?.aborted) return reject(new Error("Aborted"));
+    const timeoutId = setTimeout(() => resolve(value > 2), delay);
+    signal?.addEventListener("abort", () => {
+      clearTimeout(timeoutId);
+      reject(new Error("Aborted"));
+    });
+  });
+}
+
+async function* generateData(array) {
+  for (const item of array) yield item;
+}
 
